eed907b3717cbfc368da9d249fd47b1b82660483
kkamkou <kkamkou@gmail.com>
2011-12-22 21:25:51 +0300

    doctype correction

--- src/Vkontakte/Api.php
+++ src/Vkontakte/Api.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * Author(s): Kanstantsin A Kamkou <2ka.by>
- *
  * Licensed under the MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * @category ThirdParty
- * @package  Vkontakte
- * @author   Copyright (c) 2011 Kanstantsin A Kamkou (2ka.by)
+ * @author   Kanstantsin A Kamkou (2ka.by)
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link     https://github.com/kkamkou/ZF-Vkontakte-SDK
  */
